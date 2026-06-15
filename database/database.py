ea2b472babb1a3429e634fc47eef03d986bbc58d
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-15 22:02:05 +0330

    feat(database-database): add get_db dependency function

--- database/database.py
+++ database/database.py
@@ -7,3 +7,14 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
