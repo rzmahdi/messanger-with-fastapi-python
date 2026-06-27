a4dcf784bd004ec3898d6f587eb71cd9cebcded6
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-27 13:44:19 +0330

    fix(auth-service): fix get_current_user name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from database.database import Base, engine, get_db
 from contextlib import asynccontextmanager
 from routers import auth
-from services.auth_service import get_curent_user
+from services.auth_service import get_current_user
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -15,5 +15,5 @@ app = FastAPI(lifespan=life_span)
 app.include_router(auth.router)
 
 @app.get("/")
-def user(user: dict = Depends(get_curent_user), db=Depends(get_db)):
+def user(user: dict = Depends(get_current_user), db=Depends(get_db)):
     return {"User": user}
--- services/auth_service.py
+++ services/auth_service.py
@@ -36,7 +36,7 @@ def create_access_token(username: str, user_id: int, exp_delta: timedelta):
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def get_curent_user(token: Annotated[str, Depends(oauth2_bearer)]):
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username = payload.get("sub")
