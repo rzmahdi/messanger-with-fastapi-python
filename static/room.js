dc011e56e25f1802fae267bf735813fdf2736315
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-02 16:31:34 +0330

    fix(room-js): fix many issues

--- static/room.js
+++ static/room.js
@@ -1,23 +1,27 @@
 async function load_messages(){
-    const res = await fetch(`/rooms/${room_id}/messages?limit=20&offset=0`);
+    const res = await fetch(`/room/${room_id}/messages?limit=20&offset=0`);
     const messages = await res.json();
 
-    const container = document.getElementById("messgaes");
+    const container = document.getElementById("messages");
 
-    messages.reverse().forEach(message => {
+    messages.forEach(message => {
         add_message(message);
     });
 }
 
 function add_message(message){
-    const container = document.getElementById("messgaes");
+    const container = document.getElementById("messages");
 
     const div = document.createElement("div");
-    div.className = "mesage";
+    div.className = "message";
 
     div.innerHTML = `
-    <b>${message.username || message.user_id}</b>: ${message.content}
+    <b>${message.user.username}</b>: ${message.content}
     `;
+
+    console.log(div);
 
     container.appendChild(div);
 }
+
+load_messages();
