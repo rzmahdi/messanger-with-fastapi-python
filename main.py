b0465e55a0f6b6e2c714f26eb23037283744f84a
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-06 16:55:32 +0330

    refactor(main): remove unused imports from main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, Depends
-from database.database import Base, engine, get_db
+from fastapi import FastAPI
+from database.database import Base, engine
 from contextlib import asynccontextmanager
 from routers import auth, pages, rooms, messages, websocket
-from services.auth_service import get_current_user
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
