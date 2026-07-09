a9e4be98bb5ce9fed8b09c0d82821c63ce621968
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-09 13:21:13 +0330

    fix(rooms): correct user ID retrieval in create_room function

--- routers/rooms.py
+++ routers/rooms.py
@@ -20,7 +20,7 @@ def retrive_rooms(request: Request, room_name: str | None = None, db: Session=De
 
 @router.post("/rooms", status_code=201)
 def create_room(request: RoomCreateSchema, user: User=Depends(get_current_user), db: Session=Depends(get_db)):
-    new_room = Room(name=request.name, created_by=user.get("id"))
+    new_room = Room(name=request.name, created_by=user.id)
     db.add(new_room)
     db.commit()
     db.refresh(new_room)
