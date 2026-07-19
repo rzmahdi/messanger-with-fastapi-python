7657594b028dd776d847cef4d57eaf9ab66d2269
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-19 17:13:05 +0330

    style(auth): enhance auth.py code style

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,17 @@
 from fastapi import Depends, HTTPException, status, Request
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
-from database.schema import UserCreateSchema, Token, RefreshTokenSchema
+from database.schema import UserCreateSchema, Token, RefreshTokenSchema, UserForgotPasswordSchema
 from database.database import get_db
 from database.models import User
-from services.auth_service import hash_password, authenticate_user, create_access_token, get_current_user, create_refresh_token, get_user_from_token
+from services.auth_service import (
+    hash_password,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+    create_refresh_token,
+    get_user_from_token,
+)
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from database.models import SECURITY_QUESTIONS
@@ -19,17 +26,15 @@ def get_security_questions():
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 def register(request: UserCreateSchema, db: Session = Depends(get_db)):
-    existing_user = (
-        db.query(User)
-        .filter_by(username=request.username)
-        .first()
-    )
+    existing_user = db.query(User).filter_by(username=request.username).first()
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user allready exists!")
-    
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="user allready exists!"
+        )
+
     if not request.security_question in SECURITY_QUESTIONS:
         raise HTTPException(400, "Invalid security question!")
-    
+
     if not request.security_answer.strip():
         raise HTTPException(400, "Secuerity answer cannot be empty!")
 
@@ -37,7 +42,7 @@ def register(request: UserCreateSchema, db: Session = Depends(get_db)):
         username=request.username,
         password_hash=hash_password(request.password),
         security_question=request.security_question,
-        security_answer_hash=hash_password(request.security_answer.strip().lower())
+        security_answer_hash=hash_password(request.security_answer.strip().lower()),
     )
 
     db.add(new_user)
@@ -46,11 +51,16 @@ def register(request: UserCreateSchema, db: Session = Depends(get_db)):
 
 
 @router.post("/login", response_model=Token)
-def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session=Depends(get_db)):
+def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db),
+):
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if user is None:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "username or password are wrong!")
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED, "username or password are wrong!"
+        )
 
     access_token = create_access_token(user.username, user.id)
     refresh_token = create_refresh_token(user.username, user.id)
@@ -58,11 +68,11 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Sessio
     return {
         "access_token": access_token,
         "refresh_token": refresh_token,
-        }
+    }
 
 
 @router.post("/refresh")
-def refresh_token_endpoint(request: RefreshTokenSchema, db: Session=Depends(get_db)):
+def refresh_token_endpoint(request: RefreshTokenSchema, db: Session = Depends(get_db)):
     user = get_user_from_token(request.refresh_token, db, is_refresh=True)
 
     if not user:
@@ -78,10 +88,7 @@ def me(current_user=Depends(get_current_user)):
 
 
 @router.get("/forgot-password/{username}")
-def get_user_security_question(
-    username: str,
-    db:Session=Depends(get_db)
-    ):
+def get_user_security_question(username: str, db: Session = Depends(get_db)):
     user = db.query(User).filter_by(username=username).first()
 
     if not user:
