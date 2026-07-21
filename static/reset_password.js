22a7300962f8499802e4d5916ef00884ae4cc6fe
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-21 16:47:32 +0330

    feat(reset-password-js): add notification functions for reset password

--- static/reset_password.js
+++ static/reset_password.js
@@ -16,3 +16,50 @@ const answer_input = document.getElementById("reset-answer");
 const password_input = document.getElementById("new-password");
 
 let password_reset_status = null;
+localStorage.removeItem("reset_token");
+
+
+function show_notif(text){
+    reset_notif_modal.classList.add("show");
+    reset_notif_text.innerHTML = text;
+}
+
+function close_notif(){
+    reset_notif_modal.classList.remove("show");
+}
+
+function show_username_empty(){
+    reset_empty_username_span.classList.add("error");
+}
+function hide_username_empty(){
+    reset_empty_username_span.classList.remove("error");
+}
+
+function show_answer_empty(){
+    reset_empty_answer_span.classList.add("error");
+}
+function hide_answer_empty(){
+    reset_empty_answer_span.classList.remove("error");
+}
+
+function show_password_empty(){
+    reset_empty_password_span.classList.add("error");
+}
+function hide_password_empty(){
+    reset_empty_password_span.classList.remove("error");
+}
+
+function show_password_invalid(){
+    reset_invalid_password_span.classList.add("error");
+}
+function hide_password_invalid(){
+    reset_invalid_password_span.classList.remove("error");
+}
+
+function show_security_question(question){
+    security_question_span.classList.add("show");
+    security_question_span.textContent = question;
+}
+function hide_sequrity_question(){
+    security_question_span.classList.remove("show");
+}
