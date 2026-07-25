049f491608cf1ee851ba191bced27caa9ff224fc
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-25 16:05:24 +0330

    feat(database): change database to postgres

--- database/database.py
+++ database/database.py
@@ -1,11 +1,8 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
-
-engine = create_engine(
-    "sqlite:///database.db",
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = "postgresql+psycopg2://postgres:Mahdi11228Postgres@localhost:5432/messenger"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
