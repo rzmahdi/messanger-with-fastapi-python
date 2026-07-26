c40e55219d9f3d41510646de95f53c2f30a93456
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-26 16:10:11 +0330

    feat(models): add profile_pic field to User model for profile customization

--- database/models.py
+++ database/models.py
@@ -21,6 +21,7 @@ class User(Base):
     display_name = Column(String(40), nullable=False)
     password_hash = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.now, nullable=False)
+    profile_pic = Column(String(255), nullable=True)
     bio = Column(String(70), nullable=True)
 
     security_question = Column(
