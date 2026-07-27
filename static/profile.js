96ff1f54afe50d16f01c29cdb36bca55ebcb33f3
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-27 12:09:44 +0330

    feat(profile-js): initialize user bio display in profile

--- static/profile.js
+++ static/profile.js
@@ -2,7 +2,7 @@ const profile_pic_element = document.getElementById("profile-img");
 const image_wrapper = document.getElementById("profile-image-container");
 const back_btn = document.getElementById("back-btn");
 const profile_display_name = document.getElementById("profile-display-name");
-
+const profile_bio = document.getElementById("user-bio");
 
 const username_colors = [
     "#ffae00",
@@ -17,7 +17,7 @@ const username_colors = [
 
 
 profile_display_name.textContent = display_name;
-
+profile_bio.textContent = bio!=="None" ? bio : "";
 
 function getUserColor(){
     return username_colors[username.charCodeAt()%username_colors.length];
