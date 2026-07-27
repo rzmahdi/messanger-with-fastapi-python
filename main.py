462c978e446d8ef30f798b78977ee9b057a61edc
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-27 16:21:12 +0330

    feat(main): add user router and static files mounting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from database.database import Base, engine
 from contextlib import asynccontextmanager
-from routers import auth, pages, rooms, messages, websocket
+from routers import auth, pages, rooms, messages, websocket, user
+from fastapi.staticfiles import StaticFiles
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -11,8 +12,11 @@ async def life_span(app: FastAPI):
     print("Application Stop!")
 
 app = FastAPI(lifespan=life_span)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(auth.router)
 app.include_router(pages.router)
 app.include_router(rooms.router)
 app.include_router(messages.router)
 app.include_router(websocket.router)
+app.include_router(user.router)
