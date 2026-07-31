97fe340181478fec2217985ed6cd4c2bcc450117
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-31 13:10:42 +0330

    feat(register): add name and username validation

--- static/register.js
+++ static/register.js
@@ -6,6 +6,8 @@ const register_notif_modal = document.getElementById("register-modal-overlay-not
 const register_notif_text = document.getElementById("register-notif-modal-text");
 const register_password_invalid_span = document.getElementById("register-password-invalid");
 const register_close_notif_btn = document.getElementById("register-modal-notif-close-btn");
+const register_invalid_name = document.getElementById("invalid-name");
+const register_invalid_username = document.getElementById("invalid-username");
 
 const select = document.getElementById("register-security-question");
 const register_security_answer = document.getElementById("register-security-answer");
@@ -43,6 +45,16 @@ function question_validation(){
 function answer_validation(){
     if(register_security_answer.value.length === 0) return false;
     return true;
+}
+
+function userNameVlidation(name){
+    const pattern = /^[a-zA-Z][a-zA-Z0-9_]{2,14}$/;
+    return pattern.test(name);
+}
+
+function nameVlidation(name){
+    const pattern = /^[a-zA-Z0-9_\-\s\u0600-\u06FF\u200C]{3,40}$/;
+    return pattern.test(name);
 }
 
 function show_notif(text){
@@ -85,53 +97,67 @@ loadSecurityQuestions();
 
 register_form.addEventListener("submit", async (e)=>{
     e.preventDefault();
-    const display_name = document.getElementById("register-display-name").value;
-    const username = document.getElementById("register-username").value;
+    const display_name = document.getElementById("register-display-name").value.trim();
+    const username = document.getElementById("register-username").value.trim();
     const password = document.getElementById("register-password").value;
     const confirm_password = document.getElementById("register-confirm-password").value;
     const security_question = select.value;
     const security_answer = register_security_answer.value;
 
     if(!display_name){
-        register_empty_name_span.classList.add("error");
+        register_empty_name_span.classList.add("show");
         return
     }else{
-        register_empty_name_span.classList.remove("error");
+        register_empty_name_span.classList.remove("show");
     }
 
     if(!username){
-        register_empty_username_span.classList.add("error");
+        register_empty_username_span.classList.add("show");
         return
     }else{
-        register_empty_username_span.classList.remove("error");
+        register_empty_username_span.classList.remove("show");
     }
 
-    if(!check_password(password, confirm_password)){
-        passwords_not_match_span.classList.add("error");
+    if(!nameVlidation(display_name)){
+        register_invalid_name.classList.add("show");
         return
     }else{
-        passwords_not_match_span.classList.remove("error");
+        register_invalid_name.classList.remove("show");
+    }
+
+    if(!userNameVlidation(username)){
+        register_invalid_username.classList.add("show");
+        return
+    }else{
+        register_invalid_username.classList.remove("show");
     }
 
     if(!password_validation(password)){
-        register_password_invalid_span.classList.add("error");
+        register_password_invalid_span.classList.add("show");
         return
     }else{
-        register_password_invalid_span.classList.remove("error");
+        register_password_invalid_span.classList.remove("show");
+    }
+
+    if(!check_password(password, confirm_password)){
+        passwords_not_match_span.classList.add("show");
+        return
+    }else{
+        passwords_not_match_span.classList.remove("show");
     }
 
     if(!question_validation()){
-        question_invalid_span.classList.add("error");
+        question_invalid_span.classList.add("show");
         return
     }else{
-        question_invalid_span.classList.remove("error");
+        question_invalid_span.classList.remove("show");
     }
 
     if(!answer_validation()){
-        answer_empty_span.classList.add("error");
+        answer_empty_span.classList.add("show");
         return
     }else{
-        answer_empty_span.classList.remove("error");
+        answer_empty_span.classList.remove("show");
     }
 
 
@@ -153,6 +179,7 @@ register_form.addEventListener("submit", async (e)=>{
         show_notif("User successfuly created✅");
         register_status = true;
         localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
     }else if(response.status === 409){
         show_notif("User allready exists!❌");
     }
